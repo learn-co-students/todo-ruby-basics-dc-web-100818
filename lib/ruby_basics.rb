c7467cbb9d4672bf7af64b729716fb629abe6b58
Solee Lee <carolinesoleelee@gmail.com>
2018-09-19 22:54:40 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+  30 / 5
 end
 
-def assign_variable(value)
-  
+def assign_variable(name)
+  puts = "#{name}"
 end
 
-def argue
-  
+def argue(ha)
+  "I'm right and you are wrong!"
+
 end
 
-def greeting
-
+def greeting(greeting, name)
+   puts = "#{greeting} #{name}"
 end
 
 def return_a_value
-
+ "Nice"
 end
 
 def last_evaluated_value
-
+ "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping= "cheese")
+  puts = "#{topping}"
 end
